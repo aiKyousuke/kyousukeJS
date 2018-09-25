8f1d2f3600e72843733bd2cda3da0f80e45f0a52
aikyousuke <aisaloon.kyousuke@gmail.com>
2018-09-26 06:34:41 +0900

    18009260634

--- aiDegree.js
+++ aiDegree.js
@@ -8,8 +8,35 @@ javascript: (function(func, scr) {
 })(function($) {
     "use strict";
     
-    const dd = new Date();
-    const year = dd.getFullYear();
-    const oldYear = year - 1;
+    const nextUInt = function() {
+        let x = Math.floor(Math.random() * 1000000000);
+        let y = Math.floor(Math.random() * 1000000000);
+        let z = Math.floor(Math.random() * 1000000000);
+        let w = Math.floor(Math.random() * 100000000);
+        const t = x ^ x << 11;
+        x = y;
+        y = z;
+        z = w;
+        w = (w ^ w >>> 19) ^ (t ^ t >>> 8);
+        
+        const result = w >>> 0;
+        return result / 4294967296;
+    };
     
+    Math.randomInt = function(max) {
+        return Math.floor(max * nextUInt());
+    };
+    
+    const degree = {};
+    degree.total = 97;
+    
+    const dd = new Date();
+    degree.year = dd.getFullYear();
+    degree.oldYear = year - 1;
+    
+    const r = Math.randomInt(degree.total);
+    degree.list = [];
+    
+    $('#text').text(degree.list[r]);
+    localStorage.setItem("shogo", degree.list[r]);    
 });
