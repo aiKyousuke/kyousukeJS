0ed710c0a412f841d475783713d391ef46d41e80
aikyousuke <aisaloon.kyousuke@gmail.com>
2018-09-26 20:06:39 +0900

    1809262006

--- aiLog.js
+++ aiLog.js
@@ -27,6 +27,6 @@ javascript: (function(func, scr) {
         return Math.floor(max * nextUInt());
     };
     
-    $('body > div:eq(0)').html('tttttesttttt');
+    $('body > div:eq(0)').text('tttttesttttt');
     
 });
