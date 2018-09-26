924e2840bc7069f9269b53913653fe8a745296e5
aikyousuke <aisaloon.kyousuke@gmail.com>
2018-09-26 21:25:24 +0900

    1809262125

--- aiLog.js
+++ aiLog.js
@@ -27,6 +27,6 @@ javascript: (function(func, scr) {
         return Math.floor(max * nextUInt());
     };
     
-    $('body > div:eq(0) > div:eq(0) > div:eq(0) > a:eq(0) > img:eq(0)').attr('src', 'personal/user780/pic/ky02.jpg');
+    $('body > div:eq(0) > div:eq(0) > div:eq(0)').text('tttttesttttt');
     
 });
