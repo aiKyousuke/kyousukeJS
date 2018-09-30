614887be51c6553e1b63828b9c951714962b2226
aikyousuke <aisaloon.kyousuke@gmail.com>
2018-10-01 07:16:15 +0900

    1810010716

--- aiLog.js
+++ aiLog.js
@@ -29,12 +29,8 @@ javascript: (function(func, scr) {
     
     const mainText = $('#text').html();
     
-    const tagDiv = $('body > div:eq(0) > div:eq(0) > div:eq(1)');
-    const changeStyle = 'background-color:#ffffff;border:1px solid #ffffff;margin-top:0px;minx-width:300px;max-width:640px;padding:0px;'
-    tagDiv.attr('style', changeStyle);
-    
     const imgUrl = 'personal/user780/pic/';
-    
+
     const imgList = ['lba2121.jpg', 'rngif1.gif', 'moa3.jpg', 'kda5.jpg', 'ssa1.jpg',
                      'lba8.jpg', 'aca1.jpg', 'kda4.jpg', 'kma2.jpg', 'kma1.jpg',
                      'anime0301.jpg', 'animeex1.jpg', 'komaria01.jpg', 'anime15n01.jpg', 'rna4.jpg',
@@ -45,20 +41,38 @@ javascript: (function(func, scr) {
                      'msa2.jpg', 'kya1.jpg', 'kma4.jpg', '79942.jpg', 'lba7.jpg',
                      'lba6.jpg', 'lba5.jpg', 'lba4.jpg', 'lba3.jpg', 'lba2.jpg',
                      'lba1.jpg', 'yka2.jpg', 'kda2.jpg'];
-    
+
     const imgCount = imgList.length;
     const r = Math.randomInt(imgCount);
-    
+
     const output = imgUrl + imgList[r];
-    //$('#randimg').attr('src', output);
-    const addHtml = `<a href="http://ai-saloon.com/">` + 
-          `<img id="randimg" src="${output}" width="100%">` +
-          `</a>` +
-          `</div>` +
-          `<div style="background-color:#ffffff;border:1px solid #0080FF;margin-top:5px;minx-width:300px;max-width:600px;padding:3px;">` +
-          mainText;
-    tagDiv.html(addHtml);
     
-    const smpDiv = $('body > div:eq(0) > table:eq(0) > tbody:eq(0) > tr:eq(0) > td:eq(0)');
-    smpDiv.html('');
+    const main = function(){
+        const tagDiv = $('body > div:eq(0) > div:eq(0) > div:eq(1)');
+        const changeStyle = 'background-color:#ffffff;border:1px solid #ffffff;margin-top:0px;minx-width:300px;max-width:640px;padding:0px;'
+        tagDiv.attr('style', changeStyle);
+        //$('#randimg').attr('src', output);
+        const addHtml = `<a href="http://ai-saloon.com/">` + 
+              `<img id="randimg" src="${output}" width="100%">` +
+              `</a>` +
+              `</div>` +
+              `<div style="background-color:#ffffff;border:1px solid #0080FF;margin-top:5px;minx-width:300px;max-width:600px;padding:3px;">` +
+              mainText;
+        tagDiv.html(addHtml);
+    };
+    
+    const sub = function(){
+        const tagDiv = $('body > div:eq(0) > table:eq(0) > tbody:eq(0) > tr:eq(0) > td:eq(0)');
+        tagDiv.html('');
+    };
+    
+    const ua = navigator.userAgent;
+    if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+        sub();
+    } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+        sub();
+    } else {
+        main();
+    };
+    
 });
