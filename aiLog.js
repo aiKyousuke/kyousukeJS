5bdfe21cebcfdd14227f6a8b731c0f2aa0d75488
aikyousuke <aisaloon.kyousuke@gmail.com>
2018-10-01 09:11:30 +0900

    1810010911

--- aiLog.js
+++ aiLog.js
@@ -7,7 +7,26 @@ javascript: (function(func, scr) {
     document.body.appendChild(scr)
 })(function($) {
     "use strict";
+    
+    const nextUInt = function() {
+        let x = Math.floor(Math.random() * 1000000000);
+        let y = Math.floor(Math.random() * 1000000000);
+        let z = Math.floor(Math.random() * 1000000000);
+        let w = Math.floor(Math.random() * 100000000);
+        const t = x ^ x << 11;
+        x = y;
+        y = z;
+        z = w;
+        w = (w ^ w >>> 19) ^ (t ^ t >>> 8);
         
+        const result = w >>> 0;
+        return result / 4294967296;
+    };
+    
+    Math.randomInt = function(max) {
+        return Math.floor(max * nextUInt());
+    };    
+    
     const mainText = $('#text').html();
     
     const imgUrl = 'personal/user780/pic/';
